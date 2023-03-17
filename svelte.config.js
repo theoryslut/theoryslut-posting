4c62e6afc7cb69bc8400131ff8f4aa16c63b8e6c
James Gisele <james.may.gisele@protonmail.com>
2023-03-17 00:30:10 -0400

    gh pages....

--- svelte.config.js
+++ svelte.config.js
@@ -1,9 +1,22 @@
-import adapter from '@sveltejs/adapter-node';
+import adapter from '@sveltejs/adapter-static';
 
-/** @type {import('@sveltejs/kit').Config} */
+// let string = 'theoryslut.xyz';
+const dev = process.argv.includes('dev');
+
 const config = {
 	kit: {
-		adapter: adapter({ out: 'build' })
+		adapter: adapter({
+			// default options are shown. On some platforms
+			// these options are set automatically — see below
+			pages: 'build',
+			assets: 'build',
+			fallback: 'error.html',
+			precompress: false,
+			strict: true
+		}),
+		paths: {
+			base: dev ? '' : '/theoryslut-posting'
+		}
 	}
 };
 
