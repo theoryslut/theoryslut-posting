4e92d689ae976592617dfe398bf00c7317646d3d
theoryslut <trashfirehell@protonmail.com>
2023-08-14 00:21:28 -0400

    image problems

--- src/feed/faggot_for_sale.svelte
+++ src/feed/faggot_for_sale.svelte
@@ -1,9 +1,10 @@
 <script>
 	import faggot from '../assets/images/faggot_clothing_for_sale.png';
+	import CenterImage from '../lib/layout/CenterImage.svelte';
 </script>
 
 <p>recovered from top surgery enough to start looking around for</p>
-<img
+<CenterImage
 	src={faggot}
 	alt="screenshot of a google search result. result is for redbubble, with title 'Faggot Clothing For Sale.'"
 />
