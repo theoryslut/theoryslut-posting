1c7940be37ba435b9f6b7b2f693c9c7e46fdfa05
theoryslut <trashfirehell@protonmail.com>
2023-08-14 00:17:51 -0400

    forgot alt text

--- src/feed/faggot_for_sale.svelte
+++ src/feed/faggot_for_sale.svelte
@@ -3,5 +3,8 @@
 </script>
 
 <p>recovered from top surgery enough to start looking around for</p>
-<img src={faggot} alt="" />
+<img
+	src={faggot}
+	alt="screenshot of a google search result. result is for redbubble, with title 'Faggot Clothing For Sale.'"
+/>
 <p>never worn, etc</p>
