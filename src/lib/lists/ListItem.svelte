3c26ae658acbd8f15566b1789368a488747deb99
theoryslut <trashfirehell@protonmail.com>
2023-11-24 18:22:40 -0500

    book list

---
+++ src/lib/lists/ListItem.svelte
@@ -0,0 +1,76 @@
+<script>
+	import ChainLink from '../links/ChainLink.svelte';
+	import RainbowLink from '../links/RainbowLink.svelte';
+	export let author;
+	export let title;
+	export let comment = '';
+	export let link = '';
+</script>
+
+<li class="grow">
+	{#if link !== ''}
+		<RainbowLink href={link}
+			><b>{title}</b>
+			<span class="item-spacer">//</span>
+			<i>{author}</i>
+			<span class="item-spacer">//</span>
+			<span class="chain">
+				<svg
+					xmlns="http://www.w3.org/2000/svg"
+					width="26"
+					height="26"
+					fill="currentColor"
+					class="bi bi-link-45deg"
+					viewBox="0 0 16 16"
+				>
+					<path
+						d="M4.715 6.542 3.343 7.914a3 3 0 1 0 4.243 4.243l1.828-1.829A3 3 0 0 0 8.586 5.5L8 6.086a1.002 1.002 0 0 0-.154.199 2 2 0 0 1 .861 3.337L6.88 11.45a2 2 0 1 1-2.83-2.83l.793-.792a4.018 4.018 0 0 1-.128-1.287z"
+					/>
+					<path
+						d="M6.586 4.672A3 3 0 0 0 7.414 9.5l.775-.776a2 2 0 0 1-.896-3.346L9.12 3.55a2 2 0 1 1 2.83 2.83l-.793.792c.112.42.155.855.128 1.287l1.372-1.372a3 3 0 1 0-4.243-4.243L6.586 4.672z"
+					/>
+				</svg>
+			</span></RainbowLink
+		>
+	{:else}
+		<b>{title}</b>
+		<span class="item-spacer">//</span>
+		<i>{author}</i>
+	{/if}
+	<div class="comment">
+		{comment}
+		<slot />
+		<div />
+	</div>
+</li>
+
+<style>
+	.item-spacer {
+		padding: 0rem 0.2rem;
+	}
+
+	.chain {
+		position: relative;
+		top: 0.3em;
+	}
+
+	li {
+		list-style: none;
+		background-color: var(--base);
+		/* width: 100%; */
+		padding: 0.5em;
+		border-radius: 5px;
+	}
+
+	li:hover {
+		padding-left: 1.25em;
+	}
+
+	.grow {
+		transition: all 0.2s ease-in-out;
+	}
+
+	.grow:hover {
+		transform: scale(1.05);
+	}
+</style>
---
+++ src/routes/blog/secret/formative/+page.svelte
@@ -0,0 +1,295 @@
+<script>
+	import ListItem from '../../../../lib/lists/ListItem.svelte';
+	import ChainLink from '../../../../lib/links/ChainLink.svelte';
+</script>
+
+<svelte:head>
+	<title>formative media</title>
+</svelte:head>
+
+<h1>formative media</h1>
+<h2>ongoing list of media that has shaped my thinking</h2>
+<h4>last updated 24 November 2023</h4>
+<h5>alphabetical order by author so as not to ruffle feathers</h5>
+<h3>books & longform writing</h3>
+<ul>
+	<ListItem author="James Agee" title="Let Us Now Praise Famous Men" />
+	<ListItem
+		author="M. Jacqui Alexander"
+		title="Pedagogies of Crossing: Meditations on Feminism, Sexual Politics, Memory, and the Sacred"
+	/>
+	<ListItem
+		author="Gloria E. Anzaldúa"
+		title="Light in the Dark/Luz en lo Oscuro: Rewriting Identity, Spirituality, Reality"
+	/>
+	<ListItem author="Jane Austen" title="Pride and Prejudice" />
+	<ListItem author="Alison Bechdel" title="Fun Home" />
+	<ListItem author="b. binaohan" title="decolonizing trans/gender 101" />
+	<ListItem author="Imogen Binnie" title="Detransition Baby" />
+	<ListItem
+		author="Christopher Chitty"
+		title="Sexual Hegemony: Statecraft, Sodomy, and Capital in the Rise of the World System"
+		comment="uwu feminization of culture amirite"
+	/>
+
+	<ListItem author="Ann Cvetkovich" title="Depression: A Public Feeling" />
+	<ListItem author="Samuel R. Delany" title="The Motion of Light In Water" />
+	<ListItem author="Samuel R. Delany" title="Times Square Red, Times Square Blue" />
+	<ListItem author="Jacques Derrida" title="Archive Fever: A Freudian Impression" />
+	<ListItem author="W.E.B DuBois" title="Black Reconstruction" />
+	<ListItem author="Jackie Ess" title="Darryl" />
+	<ListItem
+		author="Stefano Harvey and Fred Moten"
+		title="The Undercommons: Fugitive Planning and Black Study"
+	/>
+	<ListItem
+		author="Bruce D. Haynes"
+		title="The Soul of Judaism: Jews of African Descent in America"
+	/>
+	<ListItem author="Zora Neale Hurston" title="Their Eyes Were Watching God" />
+	<ListItem author="Robin Wall Kimmerer" title="Braiding Sweetgrass" />
+	<ListItem author="Nella Larsen" title="Passing" />
+	<ListItem author="Bruno Latour" title="We Have Never Been Modern" />
+	<ListItem author="Karl Marx" title="Capital, vol. 1-2" comment="vol 3 still to be attempted,," />
+	<ListItem author="DA Miller" title="Jane Austen, or The Secret of Style" />
+	<ListItem author="DA Miller" title="The Novel and the Police" />
+	<ListItem author="Larry Mitchell" title="The Faggots & Their Friends Between Revolutions" />
+	<ListItem author="Never Angeline North" title="Sea Witch (series)" />
+	<ListItem
+		author="ME O'Brien"
+		title="To Abolish The Family: The Working Class Family and Gender Liberation in Capitalist Development"
+		link="https://endnotes.org.uk/articles/to-abolish-the-family.pdf"
+	/>
+	<ListItem author="Torrey Peters" title="Detransition, Baby" />
+	<ListItem author="Mihail Sebastian" title="For Two Thousand Years" />
+	<ListItem
+		author=" Eve Kosofsky Sedgwick"
+		title="Touching Feeling: Affect, Pedagogy, Performativity"
+	/>
+	<ListItem author="Carolyn Steedman" title="Dust: The Archive and Cultural History" />
+	<ListItem
+		author="Carolyn Steedman"
+		title="Strange Dislocations: Childhood and the Idea of Human Interiority, 1780–1930"
+	/>
+	<ListItem
+		author="Yosef Hayim Yerushalmi"
+		title="Freud's Moses: Judaism Terminable and Interminable"
+	/>
+	<ListItem author="Yosef Hayim Yerushalmi" title="Zakhor: Jewish History and Jewish Memory" />
+</ul>
+<h6>Anthologies</h6>
+<ListItem
+	author="various authors"
+	title="The Revolution Starts at Home: Confronting Intimate Violence Within Activist Communities"
+/>
+<ListItem
+	author="various authors"
+	title="Trap Door: Trans Cultural Production and the Politics of Visibility"
+/>
+
+<h3>games</h3>
+<ListItem author="EXOK Games" title="Celeste" link="https://www.celestegame.com/" />
+<ListItem
+	author="Porpentine Charity Heartscape"
+	title="Skulljhabit"
+	link="https://xrafstar.monster/games/twine/skulljhabit/"
+/>
+<ListItem
+	author="Porpentine Charity Heartscape"
+	title="With Those We Love Alive"
+	link="https://xrafstar.monster/games/twine/wtwla/"
+/>
+<ListItem
+	author="maddie thorson"
+	title="An Untitled Story"
+	link="https://www.mattmakesgames.com/"
+/>
+<ListItem
+	author="ZA/UM"
+	title="Disco Elysium"
+	link="https://www.reddit.com/r/DiscoElysium/comments/z6g5sx/hello_where_can_i_pirate_this_game/"
+/>
+<h3>articles & zines & shortform writing</h3>
+
+<ul>
+	<ListItem
+		author="Bench Ansfield"
+		title="Edifice Complex"
+		link="https://jewishcurrents.org/edifice-complex"
+	/>
+	<ListItem
+		author="Leo Bersani"
+		title="Is the Rectum a Grave?"
+		link="https://www.ikhtyar.org/wp-content/uploads/2014/06/Bersani-Leo-Is-The-Rectum-a-Grave.pdf"
+	/>
+	<ListItem
+		author="Erin Miles Cloud"
+		title="Toward the Abolition of the Foster System"
+		link="https://sfonline.barnard.edu/toward-the-abolition-of-the-foster-system/"
+	/>
+	<ListItem
+		author="Andrew Cornell"
+		title="A History of Vanguard"
+		link="https://libcom.org/article/history-vanguard"
+	/>
+	<ListItem
+		author="Corina Dross"
+		title="Self as Other: Reflections on Self Care"
+		link="https://github.com/rechelon/zine_library/blob/master/Theory/Self%20as%20Other%20-%20Reflections%20on%20Self%20Care.pdf"
+	/>
+	<ListItem
+		author="Endnotes"
+		title="The Logic of Gender"
+		link="https://endnotes.org.uk/translations/endnotes-the-logic-of-gender"
+		comment="really their whole archive, though I've yet to get thru it"
+	/>
+	<ListItem
+		author="Kay Gabriel"
+		title="Whose Trans Realism? Nevada and the fiction of fucking up"
+		link="https://yalereview.org/article/gabriel-nevada-trans-realism"
+	/>
+	<ListItem
+		author="Jules Gill-Peterson"
+		title="Doctors Who?  Radical lessons from the history of DIY transition"
+		link="https://thebaffler.com/salvos/doctors-who-gill-peterson"
+	/>
+	<ListItem
+		author="Donna Haraway"
+		title="A Cyborg Manifesto: Science, Technology, and Socialist-Feminism in the Late Twentieth Century"
+		link="https://warwick.ac.uk/fac/arts/english/currentstudents/undergraduate/modules/fictionnownarrativemediaandtheoryinthe21stcentury/manifestly_haraway_----_a_cyborg_manifesto_science_technology_and_socialist-feminism_in_the_....pdf"
+	/>
+	<ListItem
+		author="Irwin Keller"
+		title="Joseph's Womb: Gender Complexity in the Story of Joseph"
+		link="https://static1.squarespace.com/static/55b7fe53e4b05ac40f011280/t/62f6a9cce2246e686ec248ab/1660332522230/Keller-Josephs-Womb-2021.pdf"
+	/>
+	<ListItem
+		author="Bryn Kelly"
+		title="Other Balms, Other Gileads "
+		link="https://wewhofeeldifferently.info/journal.php#Bryn"
+	/>
+	<ListItem
+		author="Lawrence Kesteloot"
+		title="Coding Machines"
+		link="https://www.teamten.com/lawrence/writings/coding-machines/"
+	/>
+	<ListItem
+		author="Nathan Lee and Carlos Motta"
+		title="There is Tremendous Ferocity in Being Gentle"
+		link="https://wewhofeeldifferently.info/journal.php#PrEP"
+	/>
+	<ListItem
+		author="Tracey Lien"
+		title="No girls allowed: Unraveling the story behind the stereotype of video games being for boys."
+		link="https://www.polygon.com/features/2013/12/2/5143856/no-girls-allowed"
+	/>
+	<ListItem
+		author="lowendtheory"
+		title="On Audre Lorde’s Legacy and the “Self” of Self-Care, Part 1 of 3"
+		link="https://web.archive.org/web/20201116054516/https://www.lowendtheory.org/post/43457761324/on-audre-lordes-legacy-and-the-self-of"
+	>
+		<div>
+			and part two <ChainLink
+				href="https://lowendtheory.tumblr.com/post/50428216600/on-audre-lordes-legacy-and-the-self-of"
+			/>
+		</div>
+	</ListItem>
+	<ListItem
+		author="lowendtheory"
+		title="Theses on Adjunctification"
+		link="https://lowendtheory.tumblr.com/post/112138864200/theses-on-adjunctification"
+	/>
+	<ListItem
+		author="Tiona Nekkia McClodden"
+		title="My Existential Limits to the Rectification of Past Wrongs, Or, So If You See Me Crying, It’s Just a Sign That I’m Still Alive"
+		link="https://canopycanopycanopy.com/contents/my-existential-limits-to-the-rectification-of-past-wrongs"
+	/>
+	<ListItem
+		author="Fred Moten"
+		title="Blackness and Nothingness (Mysticism in the Flesh)"
+		link="http://raumstation.cc/content/archiv/poetiken_der_existenz/Moten-blackness-and-nothingness-mysticism-in-the-flesh.pdf"
+	/>
+	<ListItem
+		author="ME O'Brien"
+		title="Tracing this Body: Transsexuality, pharmaceuticals & capitalism"
+		link="https://eastbayprisonersupport.files.wordpress.com/2009/09/tracingthebody.pdf"
+	/>
+	<ListItem
+		author="Casey Plett"
+		title="Rise of the Gender Novel"
+		link="https://thewalrus.ca/rise-of-the-gender-novel/"
+	/>
+	<ListItem
+		author="Hortense Spillers"
+		title="Mama’s Baby, Papa’s Maybe: An American Grammar Book"
+		link="https://people.ucsc.edu/~nmitchel/hortense_spillers_-_mamas_baby_papas_maybe.pdf"
+	/>
+	<ListItem
+		author="Sandy Stone"
+		title="The Empire Strikes Back: A Posttranssexual Manifesto"
+		link="https://sandystone.com/empire-strikes-back.pdf"
+	/>
+	<ListItem
+		author="Stanford Encyclopedia of Philosophy"
+		title="The Concept of Religion"
+		link="https://plato.stanford.edu/entries/concept-religion/"
+	/>
+	<ListItem
+		author="Susan Stryker"
+		title="My Words to Victor Frankenstein Above the Village of Chamounix: Performing Transgender Rage"
+		link="https://watermark.silverchair.com/ddglq_1_3_237.pdf?token=AQECAHi208BE49Ooan9kkhW_Ercy7Dm3ZL_9Cf3qfKAc485ysgAAA1AwggNMBgkqhkiG9w0BBwagggM9MIIDOQIBADCCAzIGCSqGSIb3DQEHATAeBglghkgBZQMEAS4wEQQMRxHC7k6nakE4tdF0AgEQgIIDAy4lWn7lQfwY7KthgJuIiSv-cPkBgsiW847QtPXy1pPmz_MbefONVlzl2hErBqkO9qGBwEc8NXtqFN-z_r5FGUEQo2b_YSI15zQyBvGAR-ZjNv2VDUEzQjmucqTk53nPIrMZ-2yG_ynCql6eaQb777jldtUkEXFhFuzTkdEDsEXLP1-nOtmxOAvuHcEDoa2orI4KIcWGgRRt27DyIiJxoLS6_XvO-cBb4Yx6cMQgIg9QqDRj5W2-1E7Ila0cisjADPSihdxZZO160ndXiHgnozfk8eczky6SuEvF4Zm1dqbeZ40ZO_8UR3adhUUVguppvi1476fd8Z0NBzyxAm7chgjUnq5S0_vSQsW2n0YQsMrqjOoXmeAvMfpNv3LAVAOmQ5IGjWfv7gh7Nx-4fgiH-4l-e3sYKe4QKIPFHdV-SMVA5F7lV885IFUC6S6GLV0OOXRSRowpcKX8qdS1RB8TNAuFhJ1x6GCWU6JGVlX5I6Ytu2vbPCRSPMdBg_PPeWCruTeD9y8wzbbpvDAf0l5UzVqkzxBiZrsR-a61ksoqkGlFR02X8EtfqzT1sk1oEg7oJuvy3-XG02dqlaqkdqy51fZVopbzmgFbCei1iMVBKsvTXzUSBb62e3ubEYnSO2wa7Qq0_mIbjsVcoBFnJMUrOZ3k5x6_e6GI-RLtVheWIaCatcoS7KROjjEht0no9evDPKgNsLTdieWl5TQGBkLFr469rhoHDWra0Hrdl_IaCczUQqQ-w2v14vuEQwoChAF883Gx_uYmSfpJGG_xTH2F3rh7WNwckK9xtst_320YcLGTnSmZszZI6kxUSu9MCQ0r1jgpWrY0aBny9daQEBxr9gbGma4SLVcuVq4Qdsr7boVZEXMe7aDz-D3GHibHe1gCXXYrTEH-pf4YkEXGUSNr_zsxzajZcYBm0xIJ2R8ttSC-r2GtgLxhuHDmP503h4oWH34r2jPQJh-PeS7KJePlB3DP416cIJwbSjwE6Jpnl3gPSAAHjW8UiceAsuZjsaQPrKMSCA"
+	/>
+	<ListItem
+		author="Nsambu Za Suekama"
+		title="My Gender is Marronage: A Revisitation"
+		link="https://redvoice.news/my-gender-is-marronage-a-revisitation/"
+	/>
+	<ListItem
+		author="tante"
+		title="The Age of the Grift Shift"
+		link="https://tante.cc/2023/09/21/the-age-of-the-grift-shift/"
+	/>
+	<ListItem
+		author="Leah Tigers"
+		title="A Sex Close to Noise"
+		link="http://www.trickymothernature.com/asexclosetonoise.html"
+	/>
+	<ListItem
+		author="Leah Tigers"
+		title="On the Clinics and Bars of Weimar Berlin"
+		link="http://www.trickymothernature.com/insideout1.html"
+	/>
+	<ListItem
+		author="Omise'eke Natasha Tinsley"
+		title="BLACK ATLANTIC, QUEER ATLANTIC: Queer Imaginings of the Middle Passage"
+		link="https://archive.org/stream/pdfy-Bzu8YD8vTDQGsr7t/omiseeke-natasha-tinsley-black-atlantic-queer-atlantic-queer-imaginings-of-the-middle-passage%20copy_djvu.txt"
+	/>
+	<ListItem
+		author="Tourmaline"
+		title="Commencement Address at Hampshire College"
+		link="https://web.archive.org/web/20180304063307/http://www.reinagossett.com/commencement-address-hampshire-college/"
+	/>
+	<ListItem
+		author="Tourmaline and Cyrus Dunham"
+		title="Touch One Another"
+		link="https://web.archive.org/web/20180221104324/http://www.reinagossett.com/touch-one-another/"
+	/>
+	<ListItem
+		author="Jackie Wang"
+		title="Oceanic Feeling and Communist Affect"
+		link="https://loneberry.tumblr.com/post/153995404787/oceanic-feeling-and-communist-affect"
+	/>
+	<ListItem
+		author="Autumn Womack"
+		title="
+	'The Brown Bag of Miscellany': Zora Neale Hurston and the Practice of Overexposure"
+		link="https://sfonline.barnard.edu/the-brown-bag-of-miscellany/"
+	/>
+</ul>
+
+<style>
+	ul {
+		padding: 0;
+		list-style-type: none;
+	}
+</style>
